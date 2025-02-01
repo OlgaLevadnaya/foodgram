7c5f1826b33d22296aa3e0aed39247064df3459d
OlgaLevadnaya <olga@levadnaya.ru>
2025-02-02 00:59:39 +0300

    Testing on server

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,4 +1,4 @@
-# from django.conf import settings
+from django.conf import settings
 from django.shortcuts import redirect
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
@@ -6,10 +6,16 @@ from rest_framework.views import APIView
 
 class RecipeRedirectView(APIView):
     def get(self, request, *args, **kwargs):
-        print(self.kwargs)
-        print(request)
-        relative_url = reverse('api:recipes-detail',
-                               args=[self.kwargs.get('pk')])
-        redirect_url = request.build_absolute_uri(relative_url)
-        # redirect_url = f"http://{hostname}/recipes/{self.kwargs.get('pk')}"
+        hostname = request.META.get('HTTP_HOST')
+        if not hostname:
+            hostname = settings.ALLOWED_HOSTS[0]
+        redirect_url = f"http://{hostname}/recipes/{self.kwargs.get('pk')}"
         return redirect(redirect_url)
+
+        # print(self.kwargs)
+        # print(request)
+        # relative_url = reverse('api:recipes-detail',
+        #                       args=[self.kwargs.get('pk')])
+        # redirect_url = request.build_absolute_uri(relative_url)
+        # redirect_url = f"http://{hostname}/recipes/{self.kwargs.get('pk')}"
+        # return redirect(redirect_url)
